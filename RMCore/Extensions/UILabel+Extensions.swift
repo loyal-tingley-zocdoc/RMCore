896171917b12e35011d57f0c2d94f4b6f3ba37dd
Ryan Mannion <rmannion@gmail.com>
2016-05-10 17:06:09 -0400

    Add UILabel extensions

---
+++ RMCore/Extensions/UILabel+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  UILabel+Extensions.swift
+//  RMCore
+//
+//  Created by Ryan Mannion on 5/10/16.
+//  Copyright © 2016 Ryan Mannion. All rights reserved.
+//
+
+import Foundation
+
+public extension UILabel {
+    public convenience init(font: UIFont, textColor: UIColor) {
+        self.init(frame: CGRectZero)
+        
+        self.font = font
+        self.textColor = textColor
+    }
+    
+    public func enableMultiline() {
+        numberOfLines = 0
+        lineBreakMode = .ByWordWrapping
+    }
+}
