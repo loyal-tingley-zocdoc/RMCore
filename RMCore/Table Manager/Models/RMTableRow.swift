e830e52ae08dc971c477638fe9e39eff20e2b8c8
Ryan Mannion <rmannion@gmail.com>
2016-11-26 11:04:00 -0800

    Add editable/deletable properties

--- RMCore/Table Manager/Models/RMTableRow.swift
+++ RMCore/Table Manager/Models/RMTableRow.swift
@@ -17,7 +17,9 @@ public class RMTableRow {
     public var isLastRow = false
     public var indexPath: NSIndexPath?
     public var height: CGFloat?
-    
+    public var editable = false
+    public var deletable = false
+
     public init(cellClass: AnyClass = RMTableViewCell.self, userInfo: Any? = nil, delegate: AnyObject? = nil) {
         self.cellClass = cellClass
         self.userInfo = userInfo
